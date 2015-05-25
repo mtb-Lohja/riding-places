70d5fcc9adabb991231d418bbaab53948fe4fe83
Tero Teelahti <tero@teelahti.fi>
2015-05-25 22:12:37 +0300

    Move commands to package.json. Change IIS to http-server

--- http-server-iisexpress.cmd
+++
@@ -1,2 +0,0 @@
-start "IIS Express" "%programfiles(x86)%\iis express\iisexpress.exe" /path:%CD%\dist /port:8080 /systray:true
-start http://localhost:8080/
--- publish.cmd
+++
@@ -1,2 +0,0 @@
-REM See https://gist.github.com/cobyism/4730490
-git stpp dist origin gh-pages
